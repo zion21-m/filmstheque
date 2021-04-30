d65be009183da7d8ef381bff9eade4926d24bdc6
merdie <merdiemlb@gmail.com>
2021-04-30 12:35:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <div>
+      <div>
+        <a>LOGO</a>
+      </div>
+      <div>
+        <a>HOME</a>
+        <a>MOVIES</a>
+      </div>
+    </div>
+  );
+};
+export default NavBar;
